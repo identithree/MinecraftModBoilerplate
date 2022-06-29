9e688b4a868b9a45cf611df060ba7a11e2d1bdd8
Quinn Lane <hello@quinnlane.dev>
2022-06-29 03:56:40 -0400

    ✨ Update with newer code and more Kotlin (for Mixins)
    Signed-off-by: Quinn Lane <hello@quinnlane.dev>

--- src/main/kotlin/dev/quinnlane/mod/mixin/meta/DevDisclaimerMixin.kt
+++ src/main/kotlin/dev/quinnlane/mod/mixin/meta/DevDisclaimerMixin.kt
@@ -1,9 +1,30 @@
 package dev.quinnlane.mod.mixin.meta
 
-import net.minecraft.client.gui.components.
+import dev.quinnlane.mod.meta.MOD_ID
+import dev.quinnlane.mod.meta.MOD_NAME
+import dev.quinnlane.mod.meta.MOD_VERSION
 
+import net.minecraft.client.gui.hud.DebugHud
+
+import org.quiltmc.loader.api.QuiltLoader
 import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.injection.At
+import org.spongepowered.asm.mixin.injection.Inject
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
 
-@Mixin(DebugScreenOverlay::class)
+@Mixin(DebugHud::class)
 class DevDisclaimerMixin {
+    @Inject(method = ["getRightText"], at = [At("RETURN")])
+    private fun appendInfo(cir: CallbackInfoReturnable<MutableList<String>>) {
+        if (QuiltLoader.isDevelopmentEnvironment()) {
+            val messages = cir.returnValue
+            messages.add("")
+            messages.add("§a[Quilt Development Environment]")
+            messages.add("Mod: $MOD_NAME ($MOD_ID)")
+            messages.add("Version: $MOD_VERSION")
+            messages.add("§cWhat you see here may not be")
+            messages.add("§crepresentative of the final build!")
+            messages.add("")
+        }
+    }
 }
